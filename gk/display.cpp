d614540c891d383ce977a2e01a4647249510022f
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2007-05-23 12:39:35 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@371 14f08429-6e20-0410-a26c-ef81b9fd1c62

--- gk/display.cpp
+++ gk/display.cpp
@@ -3,11 +3,43 @@
 
 #include <boost/format.hpp>
 
-int setup_window(std::string title, int x, int y, int width, int height)
+bool setup_window(std::string title, int x, int y, int width, int height)
 {
 	glutInitWindowSize(width, height);
 	glutInitWindowPosition(x, y);
-	return glutCreateWindow(title.c_str());
+	
+	int window = glutCreateWindow(title.c_str());
+
+	if (window != 0)
+		atexit(cleanup_window);
+
+	return window != 0;
+}
+
+void cleanup_window()
+{
+	glutDestroyWindow(glutGetWindow());
+}
+
+bool setup_fullscreen(int width, int height, int depth, int frequency)
+{
+	glutGameModeString((boost::format("%1%x%2%:%3%")
+		% width % height % depth).str().c_str());
+
+	bool possible = glutGameModeGet(GLUT_GAME_MODE_POSSIBLE);
+
+	if (possible)
+	{
+		glutEnterGameMode();
+		atexit(cleanup_fullscreen);
+	}
+
+	return possible;
+}
+
+void cleanup_fullscreen()
+{
+	glutLeaveGameMode();
 }
 
 void reshape_window(int width, int height)
@@ -20,12 +52,4 @@ void reshape_window(int width, int height)
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-}
-
-void setup_fullscreen(int width, int height, int depth, int frequency)
-{
-	glutGameModeString((boost::format("%1%x%2%:%3%")
-		% width % height % depth).str().c_str());
-
-	glutEnterGameMode();
 }
--- gk/display.hpp
+++ gk/display.hpp
@@ -3,8 +3,12 @@
 
 #include <string>
 
-int setup_window(std::string title, int x, int y, int width, int height);
-void setup_fullscreen(int width, int height, int depth, int frequency);
+bool setup_window(std::string title, int x, int y, int width, int height);
+void cleanup_window();
+
+bool setup_fullscreen(int width, int height, int depth, int frequency);
+void cleanup_fullscreen();
+
 void reshape_window(int w, int height);
 
 #endif //DISPLAY_HPP
