bb95e3c77f5457a9d516838e0d9dc15ca90f0d81
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2007-05-29 20:13:24 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@407 14f08429-6e20-0410-a26c-ef81b9fd1c62

--- gk/helpers/bsp_entity.cpp
+++ gk/helpers/bsp_entity.cpp
@@ -1,10 +1,69 @@
 #include "bsp_entity.hpp"
 
 #include <iostream>
+#include <map>
 
-bsp_entity::bsp_entity(std::string description)
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/bind.hpp>
+
+#include <boost/spirit.hpp>
+#include <boost/spirit/actor.hpp>
+
+using namespace boost::lambda;
+using namespace boost::spirit;
+
+static std::vector<std::string> split(std::string entities)
 {
-	std::cerr << description << std::endl;
+	/*
+	std::vector<std::string> result;
+
+	rule<> set_rule
+		= conf
+
+	parse(entities.c_str(), set_rule);
+	return result;
+	*/
+	std::cerr << entities << std::endl;
+}
+
+/*
+{
+"classname" "misc_model"
+"model" "models/mapobjects/skull/skull_tilt1.md3"
+"angle" "135"
+"origin" "-972 -1908 -104"
+}
+*/
+
+bsp_entity::bsp_entity(std::string entity)
+{
+	std::map<std::string, std::string> values;
+
+	std::string key, value;
+
+	rule<> pair_rule = 
+		confix_p("\"", (+anychar_p)[assign_a(key)], "\"") 
+		>> 
+		*space_p
+		>>
+		confix_p("\"", (+anychar_p)[assign_a(value)], "\"");
+
+	rule<> row_rule =
+		*space_p >> pair_rule >> *space_p;
+
+	rule<> table_rule =
+		confix_p("{", *row_rule[insert_at_a(values, key, value)], "}");
+
+	 parse(entity.c_str(), table_rule);
+
+	 typedef std::pair<const std::string, std::string> pair;
+
+	 std::for_each(values.begin(), values.end(),
+		 std::cerr 
+			<< bind(&pair::first, _1) 
+			<< constant("=")
+			<< bind(&pair::second, _1)
+			<< constant("\n"));
 }
 
 void bsp_entity::draw() const
--- gk/helpers/bsp_entity.hpp
+++ gk/helpers/bsp_entity.hpp
@@ -2,10 +2,13 @@
 #define OBJECTS_BSP_ENTITY_HPP
 
 #include <string>
+#include <vector>
 
 class bsp_entity
 {
 public:
+	static std::vector<std::string> split(std::string entities);
+
 	bsp_entity(std::string description);
 	void draw() const;
 };
