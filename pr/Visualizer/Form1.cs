34cda1c36d873da729b096b52bcb04ed7690fe43
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2007-11-11 16:26:20 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@513 14f08429-6e20-0410-a26c-ef81b9fd1c62

--- pr/Visualizer/Form1.cs
+++ pr/Visualizer/Form1.cs
@@ -40,7 +40,7 @@ namespace Visualizer
                         if (parts.Length < 2)
                             continue;
 
-                        variables[parts[0]] = float.Parse(parts[1], CultureInfo.CurrentCulture);
+                        variables[parts[0]] = float.Parse(parts[1]);
                     }
                 }
 
@@ -69,44 +69,77 @@ namespace Visualizer
         private void toolStripButton2_Click(object sender, EventArgs e)
         {
             float scale = 0.03F;
-            int size = 15;
+            int size = 20;
             int margin = 30;
+            int title = 50;
 
-            int width = (int)(T * scale) + 2 * margin;
+            int width = (int)(T * scale) + 2 * margin + title;
             int height = n * m * size + (n + 1) * margin;
 
             Bitmap bitmap = new Bitmap(width, height);
             Graphics graphics = Graphics.FromImage(bitmap);
             graphics.FillRectangle(Brushes.White, 0, 0, width, height);
 
+            int left = margin + title;
+
             for (int i = 0; i < n; i++)
+            {
+                int top = margin + i * margin + i * m * size;
+
+                Font font = new Font(FontFamily.GenericSansSerif, 20);
+                DrawStringCentered(graphics, font, i.ToString(), margin, top, title, m * size);
+
                 for (int j = 0; j < m; j++)
                 {
-                    int  top = margin + i * margin + (i * m + j) * size;
-                    
-                    graphics.DrawRectangle(Pens.Black,
-                        margin,
-                        top,
-                        T * scale,
-                        size);
+                    int _top = top + j * size;                    
 
-                    graphics.FillRectangle(Brushes.Red,
-                        margin + r[i, j] * scale,
-                        top,
-                        (rk[i, j] - r[i, j]) * scale,
-                        size);
+                    graphics.DrawRectangle(Pens.Black, left, _top, T * scale, size);
 
-                    graphics.FillRectangle(Brushes.Green,
-                        margin + t[i, j] * scale,
-                        top,
-                        (tk[i, j] - t[i, j]) * scale,
-                        size);
+                    if (rk[i, j] - r[i, j] > 0)
+                        DrawCommunication(graphics, j.ToString(),
+                            left + r[i, j] * scale, _top, (rk[i, j] - r[i, j]) * scale, size);
+
+                    if (tk[i, j] - t[i, j] > 0)
+                        DrawComputation(graphics, j.ToString(),
+                            left + t[i, j] * scale, _top, (tk[i, j] - t[i, j]) * scale, size);
                 }
+            }
 
             if (pictureBox1.Image != null)
                 pictureBox1.Image.Dispose();
 
             pictureBox1.Image = bitmap;
+        }
+
+        private void DrawGrid(Graphics graphics)
+        {
+
+        }
+
+        private void DrawCommunication(Graphics graphics, string text, float x, int y, float width, float height)
+        {
+            DrawBlock(graphics, Brushes.Red, text, x, y, width, height);
+        }
+
+        private void DrawComputation(Graphics graphics, string text, float x, int y, float width, float height)
+        {
+            DrawBlock(graphics, Brushes.Green, text, x, y, width, height);
+        }
+
+        private void DrawBlock(Graphics graphics, Brush brush, string text, float x, int y, float width, float height)
+        {
+            graphics.FillRectangle(brush, x, y, width, height);
+            graphics.DrawRectangle(Pens.Black, x, y, width, height);
+
+            DrawStringCentered(graphics, Font, text, x, y, width, height);
+        }
+
+        private static void DrawStringCentered(Graphics graphics, Font font, string text, float x, int y, float width, float height)
+        {
+            SizeF size = graphics.MeasureString(text, font);
+
+            graphics.DrawString(text, font, Brushes.Black,
+                x + width / 2 - size.Width / 2, y + height / 2 - size.Height / 2);
         }
     }
 }
