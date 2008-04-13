4fdd78dc9bf183e221959ae9c2c11e645e8cda87
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2008-04-13 16:53:16 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@656 14f08429-6e20-0410-a26c-ef81b9fd1c62

--- put/s6/ai/problem.php
+++ put/s6/ai/problem.php
@@ -1,5 +1,5 @@
 <?
-require_once('naglowek.php');
+require_once('prolog.php');
 
 $reasons = array(
 	'--wybierz powód--',
@@ -7,22 +7,77 @@ $reasons = array(
 	'prośba o sprowadzenie filmu',
 	'inne');
 
+$submit = isset($_POST['submit']);
 $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
 $reason = filter_var($_POST['reason'], FILTER_VALIDATE_INT);
 $description = filter_var($_POST['description'], FILTER_SANITIZE_STRING);
 
-if ($email && $reason && $description)
+require_once('naglowek.php');
+?>
+<h1>Zgłoszenie problemu</h1>
+<?
+if ($submit && $email && $reason && $description)
 {
 ?>
-<p>Wysłany</p>
+<p>Wiadomość wysłana. Dziękujemy.</p>
+<p>E-mail: <?=$email?></p>
+<p>Powód: <?=$reasons[$reason]?></p>
+<p>Opis: <?=$description?></p>
 <?
 }
 else
 {
 ?>
-<form action='' method='post'>
-<input type='text' name='email' value='<?=$_POST['email']?>' />
-<select name='reason'>
+<script>
+function setErrorDisplay(element, display)
+{
+	document.getElementById(element.id +'_error').style.display = display ? 'block' : 'none';
+}
+
+function validateEmail(element)
+{
+	var regex = /^[a-zA-Z0-9\.\_\-\+]+@([a-zA-Z0-9\_\-]+\.)+[a-zA-Z]+$/;
+	var valid = regex.test(element.value);
+	setErrorDisplay(element, !valid);
+	return valid;
+}
+
+function validateReason(element)
+{
+	var valid = element.selectedIndex != 0;
+	setErrorDisplay(element, !valid);
+	return valid;
+}
+
+function validateDescription(element)
+{
+	var valid = element.value != '';
+	setErrorDisplay(element, !valid);
+	return valid;
+}
+
+function validateProblem(form)
+{
+  var email = validateEmail(form.elements['email']);
+	var reason = validateReason(form.elements['reason']);
+	var description = validateDescription(form.elements['description']);
+	
+	return email && reason && description;
+}
+
+function updateLimit(element)
+{
+	document.getElementById(element.id + '_limit').innerHTML = 50 - element.value.length;
+}
+</script>
+<form action='' method='post' onsubmit='return validateProblem(this)'>
+
+<h2>E-mail</h2>
+<input id='email' name='email' type='text' value='<?=$_POST['email']?>' />
+<div id='email_error' class='error' style='display: <?=$submit && !$email ? 'block' : 'none'?>'>Niepoprawny adres e-mail.</div>
+
+<h2>Powód</h2>
+<select id='reason' name='reason'>
 <?
 foreach ($reasons as $key => $name)
 {
@@ -31,9 +86,18 @@ foreach ($reasons as $key => $name)
 }
 ?>
 </select>
-<textarea name='description'><?=$_POST['description']?></textarea>
-<input type='submit' />
+<div id='reason_error' class='error' style='display: <?=$submit && !$reason ? 'block' : 'none'?>'>Nie wybrano powódu.</div>
+
+<h2>Opis</h2>
+<textarea id='description' name='description' onkeyup='updateLimit(this)'><?=$_POST['description']?></textarea>
+<div id='description_error' class='error' style='display: <?=$submit && !$description ? 'block' : 'none'?>'>Nie opisano problemu.</div>
+<div id='description_hint' class='hint'>Pozostało <span id='description_limit'></span> znaków.<div>
+
+<input type='submit' name='submit' value='Wyślij' />
 </form>
+<script>
+updateLimit(document.getElementById('description'));
+</script>
 <?
 }
 require_once('stopka.php');
