d6f1f5b0f764eb6125b10bb7ec85ea4b7d0c39b7
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2010-02-23 13:40:29 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@725 14f08429-6e20-0410-a26c-ef81b9fd1c62

--- sknd/drupal/modules/interface/templates/one_column/one_column.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $Id: one_column.tpl.php,v 1.1 2009/09/15 03:38:45 techsoldaten Exp $
-?>
-<div id="interface_startform">
-  <div rid="region_zero" class="region_zero">
-    <!-- region -->
-  </div>
-</div>
