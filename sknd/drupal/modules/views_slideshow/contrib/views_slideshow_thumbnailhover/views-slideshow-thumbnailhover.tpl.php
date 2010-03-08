8182ba7c33d606e2bd26bdaf57446edbf05a3b56
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2010-03-08 15:14:08 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@726 14f08429-6e20-0410-a26c-ef81b9fd1c62

---
+++ sknd/drupal/modules/views_slideshow/contrib/views_slideshow_thumbnailhover/views-slideshow-thumbnailhover.tpl.php
@@ -0,0 +1,42 @@
+<?php
+// $Id: views-slideshow-thumbnailhover.tpl.php,v 1.1.2.1.2.5 2009/11/17 05:55:19 redndahead Exp $
+
+/**
+ *  @file
+ *  Views Slideshow: Thumbnail Hover
+ */
+  if ($options['thumbnailhover']['controls'] == 1) {
+    print theme('views_slideshow_thumbnailhover_controls', $id, $view, $options);
+  }
+  
+  if ($options['thumbnailhover']['image_count'] == 1) {
+    print theme('views_slideshow_thumbnailhover_image_count', $id, $view, $options);
+  }
+  
+  $teaser = ($options['thumbnailhover']['hover_breakout'] == 'teaser' ? TRUE : FALSE);
+  $output = '';
+  $view_teasers = FALSE;
+
+  // As we're using the 'thumbnail hover' mode, then we need to display all the view thumbnails.
+  $view_teasers = theme('views_slideshow_thumbnailhover_breakout_teasers', $view, $rows, $id, $options);
+  if (!$options['thumbnailhover']['teasers_last']) {
+    $output .= $view_teasers;
+  }
+
+  // These are hidden elements, used to cycle through the main div
+  $hidden_elements = theme('views_slideshow_thumbnailhover_no_display_section', $view, $rows, $id, $options, $teaser);
+  $output .= theme('views_slideshow_main_section', $id, $hidden_elements, 'thumbnailhover');
+
+  if ($view_teasers && $options['thumbnailhover']['teasers_last']) {
+    $output .= $view_teasers;
+  }
+  if ($options['thumbnailhover']['controls'] == 2) {
+    print theme('views_slideshow_thumbnailhover_controls',$id, $view, $options);
+  }
+  
+  if ($options['thumbnailhover']['image_count'] == 2) {
+    print theme('views_slideshow_thumbnailhover_image_count', $id, $view, $options);
+  }
+  print $output;
+
+?>
