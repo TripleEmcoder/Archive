04590dbacebca1568a7b5f349c80212b946bb2af
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2010-10-07 16:03:41 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@739 14f08429-6e20-0410-a26c-ef81b9fd1c62

--- put/s9/miabsr/Node/Program.cs
+++ put/s9/miabsr/Node/Program.cs
@@ -2,13 +2,93 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
 
 namespace Node
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            switch (args[0])
+            {
+                case "send":
+                    {
+                        string key;
+                        string[][] data;
+
+                        using (var stream = Console.OpenStandardInput())
+                            RetrievePacket(stream, out key, out data);
+
+                        using (var client = new TcpClient())
+                        {
+                            client.Connect(GetEndpoint(args[1], args[2]));
+                            using (var stream = client.GetStream())
+                                SendPacket(stream, key, data);
+                        }
+                    }
+                    break;
+
+                case "receive":
+                    {
+                        string key;
+                        string[][] data;
+
+                        var listener = new TcpListener(GetEndpoint(args[1], args[2]));
+
+                        listener.Start();
+
+                        while (true)
+                        {
+                            using (var client = listener.AcceptTcpClient())
+                            using (var stream = client.GetStream())
+                                RetrievePacket(stream, out key, out data);
+
+                            using (var stream = Console.OpenStandardOutput())
+                                SendPacket(stream, key, data);
+                        }
+                    }
+
+                case "process":
+                    {
+                        while (true)
+                        {
+                        }
+                    }
+            }
+
+        }
+
+        private static IPEndPoint GetEndpoint(string ip, string port)
+        {
+            return new IPEndPoint(IPAddress.Parse(ip), int.Parse(port));
+        }
+
+        private static void RetrievePacket(Stream stream, out string key, out string[][] data)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                key = reader.ReadLine();
+
+                var rows = reader.ReadToEnd().Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                data = new string[rows.Length][];
+
+                for (int i = 0; i < rows.Length; i++)
+                    data[i] = rows[i].Split(';');
+            }
+        }
+
+        private static void SendPacket(Stream stream, string key, string[][] data)
+        {
+            using (var writer = new StreamWriter(stream))
+            {
+                writer.WriteLine(key);
+
+                for (int i = 0; i < data.Length; i++)
+                    writer.WriteLine(string.Join(";", data[i]));
+            }
         }
     }
 }
