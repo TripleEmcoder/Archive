4064341e2bcfcf349fb6e5f14740076db6185a33
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2010-12-30 13:27:50 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@762 14f08429-6e20-0410-a26c-ef81b9fd1c62

---
+++ put/s9/ezi/Palindromader/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Palindromader
+{
+    class Program
+    {
+        class Vertex
+        {
+            public Dictionary<char, Vertex> Succesors { get; private set; }
+            public Dictionary<string, bool> Texts { get; private set; }
+
+            public Vertex()
+            {
+                Succesors = new Dictionary<char, Vertex>();
+                Texts = new Dictionary<string, bool>();
+            }
+        }
+
+        private static void Build(Vertex root, string text)
+        {
+            foreach (var start in Enumerable.Range(0, text.Length))
+            {
+                var vertex = root;
+                var index = start;
+
+                while (index < text.Length && vertex.Succesors.ContainsKey(text[index]))
+                {
+                    vertex = vertex.Succesors[text[index++]];
+                    vertex.Texts[text] = true;
+                }
+
+                while (index < text.Length)
+                {
+                    vertex.Succesors[text[index]] = new Vertex();
+                    vertex = vertex.Succesors[text[index++]];
+                    vertex.Texts[text] = true;
+                }
+            }
+        }
+
+        private static void Traverse(Vertex root, int count, ref string result)
+        {
+            Traverse(root, count, ref result, new List<char>());
+        }
+
+        private static void Traverse(Vertex vertex, int count, ref string result, List<char> buffer)
+        {
+            Console.WriteLine("{0} {1}", new string(buffer.ToArray()), vertex.Texts.Count);
+
+            foreach (var succesor in vertex.Succesors)
+            {
+                if (succesor.Value.Texts.Count == count)
+                {
+                    buffer.Add(succesor.Key);
+                    
+                    if (buffer.Count > result.Length)
+                        result = new string(buffer.ToArray());
+
+                    Traverse(succesor.Value, count, ref result, buffer);
+                    buffer.RemoveAt(buffer.Count - 1);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter text: ");
+                var text = Console.ReadLine();
+
+                if (text == null)
+                    break;
+
+                text = text.ToLower().Replace(" ", "");
+                Console.WriteLine("Processing text: {0}", text);
+
+                var root = new Vertex();
+
+                Build(root, text);
+                Build(root, new string(text.Reverse().ToArray()));
+
+                var result = "";
+                Traverse(root, 2, ref result);
+                Console.WriteLine("Longest result: {0}", result);
+            }
+        }
+    }
+}
+
