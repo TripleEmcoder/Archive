aed9d11c6b088707c44730f4f9a6add6e3915d6a
marcin.mikolajczak <marcin.mikolajczak@14f08429-6e20-0410-a26c-ef81b9fd1c62>
2011-05-04 00:19:30 +0000

    git-svn-id: https://forge.caliper.pl/scm/svn/private-memfis@777 14f08429-6e20-0410-a26c-ef81b9fd1c62

--- other/FlightFinder/FlightFinder/Program.cs
+++ other/FlightFinder/FlightFinder/Program.cs
@@ -7,69 +7,179 @@ using Db4objects.Db4o.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
 using QuickGraph;
 using QuickGraph.Algorithms;
 
 namespace FlightFinder
 {
-    class FromPicker
+    class Search
     {
-        private readonly IWebElement open;
-        private readonly IWebElement airports;
-        private readonly IWebElement close;
+        private readonly RemoteWebDriver driver;
+        private readonly IWebElement accept;
+        private readonly IWebElement search;
 
-        public FromPicker(RemoteWebDriver driver)
+        public Search(RemoteWebDriver driver)
         {
-            open = driver.FindElement(By.CssSelector("#fromDD"));
-            airports = driver.FindElement(By.CssSelector("#fromRoutes"));
-            close = airports.FindElement(By.CssSelector(".closePopup"));
+            this.driver = driver;
+            accept = driver.FindElement(By.CssSelector("#termsAccept"));
+            search = driver.FindElement(By.CssSelector(".bookFlightButton"));
         }
 
-        public IEnumerable<string> GetAirportCodes()
+        public void Submit()
         {
-            open.Click();
-
-            foreach (var airport in airports.FindElements(By.CssSelector("a.outRoute")))
-                yield return airport.GetAttribute("title");
-
-            open.Click();
-            close.Click();
-        }
-
-        public void PickAirportByCode(string code)
-        {
-            open.Click();
-            airports.FindElement(By.XPath(string.Format(".//a[@title='{0}']", code))).Click();
+            accept.Click();
+            driver.ExecuteScript("window.confirm = function(text) { return  true; }");
+            search.Click();
+            driver.ExecuteScript("window.confirm = null");
         }
     }
 
-    class ToPicker
+    class Picker
     {
-        private readonly IWebElement open;
+        private readonly IWebElement openAirports;
         private readonly IWebElement airports;
-        private readonly IWebElement close;
+        private readonly IWebElement closeAirports;
+        private readonly IWebElement openCalendar;
+        private readonly IWebElement calendar;
 
-        public ToPicker(RemoteWebDriver driver)
+        public Picker(IWebElement openAirports, IWebElement airports, IWebElement closeAirports, IWebElement openCalendar, IWebElement calendar)
         {
-            open = driver.FindElement(By.CssSelector("#toDD"));
-            airports = driver.FindElement(By.CssSelector("#toRoutes"));
-            close = driver.FindElement(By.CssSelector("#body_bookingFormTab1"));
+            this.openAirports = openAirports;
+            this.airports = airports;
+            this.closeAirports = closeAirports;
+            this.openCalendar = openCalendar;
+            this.calendar = calendar;
         }
 
         public IEnumerable<string> GetAirportCodes()
         {
-            open.Click();
+            openAirports.Click();
+            openAirports.Click();
 
             foreach (var airport in airports.FindElements(By.CssSelector("a.outRoute")))
                 yield return airport.GetAttribute("title");
 
-            close.Click();
+            closeAirports.Click();
         }
 
         public void PickAirportByCode(string code)
         {
-            open.Click();
+            openAirports.Click();
+            openAirports.Click();
             airports.FindElement(By.XPath(string.Format(".//a[@title='{0}']", code))).Click();
+        }
+
+        public void PickWhen(DateTime when)
+        {
+            openCalendar.Click();
+            new SelectElement(calendar.FindElement(By.CssSelector(".ui-datepicker-year"))).SelectByValue(when.Year.ToString());
+            new SelectElement(calendar.FindElement(By.CssSelector(".ui-datepicker-month"))).SelectByValue((when.Month - 1).ToString());
+            var group = calendar.FindElement(By.CssSelector(".ui-datepicker-group-first"));
+            group.FindElement(By.XPath(string.Format(".//a[text()='{0}']", when.Day))).Click();
+        }
+    }
+
+    class FromPicker : Picker
+    {
+        public FromPicker(RemoteWebDriver driver)
+            : base(
+            driver.FindElement(By.CssSelector("#fromDD")),
+            driver.FindElement(By.CssSelector("#fromRoutes")),
+            driver.FindElement(By.CssSelector("#fromRoutes .closePopup")),
+            driver.FindElement(By.CssSelector("#departureInput")),
+            driver.FindElement(By.CssSelector("#ui-datepicker-div")))
+        {
+        }
+    }
+
+    class ToPicker : Picker
+    {
+        public ToPicker(RemoteWebDriver driver)
+            : base(
+            driver.FindElement(By.CssSelector("#toDD")),
+            driver.FindElement(By.CssSelector("#toRoutes")),
+            driver.FindElement(By.CssSelector("#body_bookingFormTab1")),
+            driver.FindElement(By.CssSelector("#arrivalInput")),
+            driver.FindElement(By.CssSelector("#ui-datepicker-div")))
+        {
+        }
+    }
+
+    class Planner
+    {
+        private readonly Func<IWebElement> table;
+        private readonly Func<IWebElement> next;
+
+        public Planner(Func<IWebElement> table, Func<IWebElement> next)
+        {
+            this.table = table;
+            this.next = next;
+        }
+
+        public IEnumerable<FlightTag> GetInfos()
+        {
+            foreach (var cell in table().FindElements(By.XPath(".//th[count(.//b)=2]")))
+            {
+                var cellParts = cell.Text.Split(new[] { "\r\n" }, StringSplitOptions.None);
+                var whenParts = cellParts[0].Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                var when = whenParts[0] + ", " + whenParts[2] + " " + whenParts[1] + ", 20" + whenParts[3];
+                var priceParts = cellParts[2].Split(new[] { " " }, StringSplitOptions.None);
+                yield return new FlightTag(DateTime.Parse(when), double.Parse(priceParts[0]) * GetCurrencyRatio(priceParts[1]));
+            }
+
+        }
+
+        private static double GetCurrencyRatio(string currency)
+        {
+            switch (currency)
+            {
+                case "PLN":
+                    return 1.0;
+                case "EUR":
+                    return 3.9;
+                case "GBP":
+                    return 4.4;
+                case "NOK":
+                    return 0.5;
+            }
+
+            throw new NotSupportedException(currency);
+        }
+
+        public void ShowNextWeek()
+        {
+            next().Click();
+        }
+    }
+
+    class FromPlanner : Planner
+    {
+        public FromPlanner(RemoteWebDriver driver)
+            : base(
+            () => driver.FindElement(By.CssSelector("#ttable1")),
+            () => driver.FindElement(By.CssSelector("#AvailabilityInputFRSelectView_LinkButtonNextWeek1")))
+        {
+        }
+
+        public static bool Exists(RemoteWebDriver driver)
+        {
+            return driver.FindElements(By.CssSelector("#ttable1")).Any();
+        }
+    }
+
+    class ToPlanner : Planner
+    {
+        public ToPlanner(RemoteWebDriver driver)
+            : base(
+            () => driver.FindElement(By.CssSelector("#ttable2")),
+            () => driver.FindElement(By.CssSelector("#AvailabilityInputFRSelectView_LinkButtonNextWeek2")))
+        {
+        }
+
+        public static bool Exists(RemoteWebDriver driver)
+        {
+            return driver.FindElements(By.CssSelector("#ttable2")).Any();
         }
     }
 
@@ -78,6 +188,51 @@ namespace FlightFinder
         public Route(string source, string target)
             : base(source, target)
         {
+        }
+    }
+
+    class FlightTag
+    {
+        private readonly DateTime when;
+        private readonly double price;
+
+        public FlightTag(DateTime when, double price)
+        {
+            this.when = when;
+            this.price = price;
+        }
+
+        public DateTime When
+        {
+            get { return when; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}", when, price);
+        }
+    }
+
+    class Flight : TaggedEdge<string, FlightTag>
+    {
+        public Flight(string source, string target, FlightTag tag)
+            : base(source, target, tag)
+        {
+        }
+
+        public Flight(string source, string target, DateTime when, double price)
+            : base(source, target, new FlightTag(when, price))
+        {
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", base.ToString(), Tag);
         }
     }
 
@@ -90,51 +245,145 @@ namespace FlightFinder
                 if (!cache.AsQueryable<Route>().Any())
                 {
                     using (var driver = new FirefoxDriver())
-                    {
-                        driver.Url = "http://www.ryanair.com/en";
-                        var fromPicker = new FromPicker(driver);
-                        var toPicker = new ToPicker(driver);
+                        LoadRoutes(cache, driver);
 
-                        foreach (var fromCode in fromPicker.GetAirportCodes().ToArray())
-                        {
-                            fromPicker.PickAirportByCode(fromCode);
-
-                            foreach (var toCode in toPicker.GetAirportCodes().ToArray())
-                            {
-                                var route = new Route(fromCode, toCode);
-                                Console.WriteLine(route);
-                                cache.Store(route);
-                            }
-                        }
-                    }
+                    cache.Commit();
                 }
 
-                var graph = new BidirectionalGraph<string, Edge<string>>();
-                var airports = new[] {"POZ", "WRO", "BZG"};
-                var fromSuffix = "-FROM";
-                var toSuffix = "-IN";
+                var localAirports = new[] { "POZ", "WRO", "BZG" };
+                var extraAirports = new[] { "NRN" };
+                var airports = localAirports.Concat(extraAirports);
+                var from = "_FROM";
+                var to = "_TO";
+                var when = new DateTime(2011, 06, 2);
+                var weeks = 4;
 
                 var allRoutes = cache.AsQueryable<Route>().ToArray();
+                var fromRoutes = allRoutes.Where(route => airports.Contains(route.Source)).ToArray();
+                var toRoutes = allRoutes.Where(route => airports.Contains(route.Target)).ToArray();
 
-                var fromRoutes = allRoutes
-                    .Where(route => airports.Contains(route.Source))
-                    .Select(route => new Route(route.Source + fromSuffix, route.Target));
+                using (var driver = new FirefoxDriver())
+                {
+                    foreach (var routes in new[] { fromRoutes, toRoutes })
+                        foreach (var route in routes)
+                        {
+                            if (route.Source == "WRO" && route.Target == "CIA")
+                                continue;
 
-                var toRoutes = allRoutes
-                    .Where(route => airports.Contains(route.Target))
-                    .Select(route => new Route(route.Source, route.Target + toSuffix));
+                            if (route.Source == "CIA" && route.Target == "WRO")
+                                continue;
 
-                var otherRoutes = allRoutes
-                    .Where(route => !airports.Contains(route.Source))
-                    .Where(route => !airports.Contains(route.Target));
+                            if (route.Source == "AOC" || route.Target == "AOC")
+                                continue;
 
-                foreach (var routes in new[] { fromRoutes, toRoutes, otherRoutes })
-                    graph.AddVerticesAndEdgeRange(routes);
+                            if (route.Source == "TSF" || route.Target == "TSF")
+                                continue;
 
-                foreach (var fromAirport in airports)
-                    foreach (var toAirport in airports)
-                        foreach (var path in graph.RankedShortestPathHoffmanPavley(edge => 1, fromAirport + fromSuffix, toAirport + toSuffix, 10))
-                            Console.WriteLine(string.Join(", ", path));
+                            if (route.Source == "VCE" || route.Target == "VCE")
+                                continue;
+
+                            var flights = cache.AsQueryable<Flight>()
+                                .Where(flight => flight.Source == route.Source && flight.Target == route.Target);
+
+                            if (!flights.Any())
+                            {
+                                LoadFlights(cache, route, driver, when, weeks);
+                                cache.Commit();
+                            }
+                        }
+                }
+
+                var allFlights = cache.AsQueryable<Flight>().ToArray();
+                var fromFlights = allFlights.Where(flight => fromRoutes.Any(route => flight.Source == route.Source && flight.Target == route.Target));
+                var toFlights = allFlights.Where(flight => toRoutes.Any(route => flight.Source == route.Source && flight.Target == route.Target));
+
+                var graph = new BidirectionalGraph<string, Edge<string>>();
+                graph.AddVerticesAndEdgeRange(fromFlights);
+                graph.AddVerticesAndEdgeRange(toFlights);
+                graph.AddVerticesAndEdgeRange(allRoutes.Where(route => !fromRoutes.Contains(route) && !toRoutes.Contains(route)));
+                graph.AddVerticesAndEdgeRange(localAirports.Select(airport => new Flight(from, airport, DateTime.MinValue, 0)));
+                graph.AddVerticesAndEdgeRange(localAirports.Select(airport => new Flight(airport, to, DateTime.MaxValue, 0)));
+
+                foreach (var path in graph.RankedShortestPathHoffmanPavley(edge => edge is Flight ? (edge as Flight).Tag.Price : 100000, from, to, 30))
+                    Console.WriteLine(string.Join(", ", path));
+            }
+        }
+
+        private static void LoadFlights(IEmbeddedObjectContainer cache, Route route, FirefoxDriver driver, DateTime when, int weeks)
+        {
+            var fromFlights = cache.AsQueryable<Flight>()
+                .Where(flight => flight.Source == route.Source && flight.Target == route.Target);
+
+            foreach (var flight in fromFlights)
+                cache.Delete(flight);
+
+            var toFlights = cache.AsQueryable<Flight>()
+                .Where(flight => flight.Source == route.Target && flight.Target == route.Source);
+
+            foreach (var flight in toFlights)
+                cache.Delete(flight);
+
+            driver.Url = "http://www.ryanair.com/en";
+            driver.ExecuteScript("window.moveTo(0, 0); window.resizeTo(1280, 1024);");
+            var fromPicker = new FromPicker(driver);
+            var toPicker = new ToPicker(driver);
+            var search = new Search(driver);
+
+            fromPicker.PickAirportByCode(route.Source);
+            toPicker.PickAirportByCode(route.Target);
+            fromPicker.PickWhen(when);
+            toPicker.PickWhen(when);
+            search.Submit();
+
+            var fromPlanner = new FromPlanner(driver);
+            var toPlanner = new ToPlanner(driver);
+
+            foreach (var week in Enumerable.Range(1, weeks))
+            {
+                if (FromPlanner.Exists(driver))
+                    foreach (var info in fromPlanner.GetInfos())
+                    {
+                        var flight = new Flight(route.Source, route.Target, info);
+                        Console.WriteLine(flight);
+                        cache.Store(flight);
+                    }
+
+                if (ToPlanner.Exists(driver))
+                    foreach (var info in toPlanner.GetInfos())
+                    {
+                        var flight = new Flight(route.Target, route.Source, info);
+                        Console.WriteLine(flight);
+                        cache.Store(flight);
+                    }
+
+                if (week < weeks && FromPlanner.Exists(driver) && ToPlanner.Exists(driver))
+                {
+                    toPlanner.ShowNextWeek();
+                    fromPlanner.ShowNextWeek();
+                }
+            }
+        }
+
+        private static void LoadRoutes(IEmbeddedObjectContainer cache, FirefoxDriver driver)
+        {
+            foreach (var route in cache.AsQueryable<Route>())
+                cache.Delete(route);
+
+            driver.Url = "http://www.ryanair.com/en";
+            driver.ExecuteScript("window.moveTo(0, 0); window.resizeTo(1280, 1024);");
+            var fromPicker = new FromPicker(driver);
+            var toPicker = new ToPicker(driver);
+
+            foreach (var fromCode in fromPicker.GetAirportCodes().ToArray())
+            {
+                fromPicker.PickAirportByCode(fromCode);
+
+                foreach (var toCode in toPicker.GetAirportCodes().ToArray())
+                {
+                    var route = new Route(fromCode, toCode);
+                    Console.WriteLine(route);
+                    cache.Store(route);
+                }
             }
         }
     }
